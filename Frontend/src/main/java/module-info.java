0730f7d1b7b642c1b1f13f2405189e34e3e3ef19
Khushi Gajjar <khushigajjar218@gmail.com>
2025-11-24 16:16:20 +0100

    Login and Dashboard

--- Frontend/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module expensetracker.frontend {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens expensetracker.frontend to javafx.fxml;
-    exports expensetracker.frontend;
-}
